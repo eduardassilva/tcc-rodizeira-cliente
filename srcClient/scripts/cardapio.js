3837b076432f26494bffaab1a933df77ce3058f1
Eduarda Silva <52262385+eduardassilva@users.noreply.github.com>
2020-12-14 15:30:41 -0300

    Add files via upload

--- srcClient/scripts/cardapio.js
+++ srcClient/scripts/cardapio.js
@@ -1,45 +1,41 @@
 function CreateDivItemCardapio(nameItem, descItem, tagString, itemID){
+
+    alert('cardapio1 - ' + nameItem + '//' + descItem + '//' + tagString + '//' + itemID);
+
     var divItem = document.createElement('div');
     var divCheckbox = document.createElement('div');
     var inputCheckbox = document.createElement('input');
-    /*var divImg = document.createElement('div');
-    var img = document.createElement('img');*/
     var divName = document.createElement('div');
     var pName = document.createElement('p');
     var divDesc = document.createElement('div');
     var pDesc = document.createElement('p');
     var divTags = document.createElement('div');
     var labelTags = document.createElement('label');
-
+    alert('cardapio2');
     divItem.className = 'cardapioItemDiv';
     divCheckbox.className = 'cardapioPedir';
     inputCheckbox.className = 'checkboxPedir';
-    /*divImg.className = 'cardapioImg';
-    img.className = 'imgCardapio';*/
     divName.className = 'cardapioName';
     divDesc.className = 'cardapioDescDiv';
     divTags.className = 'cardapioTag';
-
+    alert('cardapio3');
     inputCheckbox.id = itemID;
     inputCheckbox.type = 'checkbox';
-
-    /*img.src = 'img/meatIcon.png';*/
-
+    alert('cardapio4');
     pName.innerHTML = nameItem;
     pDesc.innerHTML = descItem;
     labelTags.innerHTML = tagString.replaceAll('~', '\n');
-
+    alert('cardapio5');
     document.getElementById('itemsCardapio').appendChild(divItem);
     divItem.appendChild(divCheckbox);
     divCheckbox.appendChild(inputCheckbox);
-    /*divItem.appendChild(divImg);
-    divImg.appendChild(img);*/
     divItem.appendChild(divName);
     divName.appendChild(pName);
     divItem.appendChild(divDesc);
     divDesc.appendChild(pDesc);
     divItem.appendChild(divTags);
     divTags.appendChild(labelTags);
+    alert('cardapio6');
 }
 
 function PedirCardapio(){
